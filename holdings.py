a01144a0b93089fa21486108b954a41cb8900dac
alexey_ananchenko <ananchenko_as@mail.ru>
2022-11-16 23:37:16 +0300

    корректировка под PEP8

--- holdings.py
+++ holdings.py
@@ -6,21 +6,21 @@
 
 import pandas as pd
 
+
 POINT = 'Точка доставки'
+PAYMENT = 'Плательщик'
+HOLDING = 'Холдинг'
+M_HOLDING = 'Основной холдинг'
+CODES = 'Коды'
+
 
 def main():
     excel = pd.ExcelFile('Исходники/Холдинги-Резервы.xlsx')
     df_full = excel.parse('Точка доставки-Холдинг')
     df = pd.concat([
-        df_full[["Точка доставки", "Основной холдинг"]].rename(
-            columns={"Точка доставки": "Коды"}
-        ),
-        df_full[["Плательщик", "Основной холдинг"]].rename(
-            columns={"Плательщик": "Коды"}
-        ),
-        df_full[["Холдинг", "Основной холдинг"]].rename(
-            columns={"Холдинг": "Коды"}
-        )],
+        df_full[[POINT, M_HOLDING]].rename(columns={POINT: CODES}),
+        df_full[[PAYMENT, M_HOLDING]].rename(columns={PAYMENT: CODES}),
+        df_full[[HOLDING, M_HOLDING]].rename(columns={HOLDING: CODES})],
         ignore_index=True
     ).drop_duplicates()
     result_data = {}
@@ -37,7 +37,7 @@ def main():
                 cleared_values.append('Удалить строку')
         result_data[column] = cleared_values
     df_result = pd.DataFrame(result_data)
-    df_result[df_result['Коды'] != 'Удалить строку'].to_excel(
+    df_result[df_result[CODES] != 'Удалить строку'].to_excel(
         'Результаты/Холдинги.xlsx',
         index=False
     )
