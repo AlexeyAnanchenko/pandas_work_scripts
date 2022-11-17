cf8c144ac3ee9cf3162c4222e2e66380c3faeb59
alexey_ananchenko <ananchenko_as@mail.ru>
2022-11-18 00:01:34 +0300

    добавлен главный холдинг и его наименование в резервы

--- reserve.py
+++ reserve.py
@@ -5,9 +5,11 @@
 
 import pandas as pd
 from remains import get_filtered_df
+from holdings import NAME_M_HOLDING, M_HOLDING, CODES
 
 
 EMPTY_ROWS = 2
+RESERVE_HOLDING = 'Наименование'
 
 WAREHOUSE = {
     '    800WHDIS': 'Краснодар',
@@ -24,7 +26,10 @@ def main():
     )
     df = get_filtered_df(excel, WAREHOUSE, skiprows=EMPTY_ROWS)
     holdings = pd.ExcelFile('Результаты/Холдинги.xlsx').parse()
-    df = df.merge(holdings, on='Код холдинга', how='left')
+    df = df.merge(holdings, on=CODES, how='left')
+    idx = df[df[M_HOLDING].isnull()].index
+    df.loc[idx, M_HOLDING] = df.loc[idx, CODES]
+    df.loc[idx, NAME_M_HOLDING] = df.loc[idx, RESERVE_HOLDING]
     df.to_excel('Результаты/Резервы.xlsx', index=False)
 
 
