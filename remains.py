3f72a79366096edab16873f53db62bb3ddb65c71
alexey_ananchenko <ananchenko_as@mail.ru>
2022-11-19 18:15:00 +0300

    в remains вынес все столбцы в константы

--- remains.py
+++ remains.py
@@ -11,6 +11,10 @@ AVIALABLE_REST = 'Доступно (уч.ЕИ) '
 RESERVE = 'Мягкие + жёсткие резервы (уч.ЕД)'
 QUOTA = 'Остаток невыбранного резерва (уч.ЕИ)'
 FREE_REST = 'Свободный остаток (уч.ЕИ)'
+WHS = 'Склад'
+EAN = 'EAN'
+NAME = 'Наименование'
+LINK = 'Сцепка'
 WAREHOUSE = {
     '800WHDIS': 'Краснодар',
     '803WHDIS': 'Пятигорск',
@@ -22,8 +26,8 @@ WAREHOUSE = {
 
 def get_filtered_df(excel, dict_warehouses, skiprows=0):
     full_df = excel.parse(skiprows=skiprows)
-    filter_df = full_df[full_df['Склад'].isin(list(dict_warehouses.keys()))]
-    return filter_df.replace({'Склад': dict_warehouses})
+    filter_df = full_df[full_df[WHS].isin(list(dict_warehouses.keys()))]
+    return filter_df.replace({WHS: dict_warehouses})
 
 
 def main():
@@ -37,13 +41,13 @@ def main():
         filter_df[FULL_REST] - filter_df[AVIALABLE_REST]
     )
 
-    yug_df = filter_df.groupby(['Склад', 'EAN']).agg({
+    yug_df = filter_df.groupby([WHS, EAN]).agg({
         FULL_REST: 'sum',
         RESERVE: 'sum',
         AVIALABLE_REST: 'sum',
         QUOTA: 'max'
     }).reset_index()
-    yug_df.insert(0, 'Сцепка', yug_df['Склад'] + yug_df['EAN'].map(str))
+    yug_df.insert(0, LINK, yug_df[WHS] + yug_df[EAN].map(str))
     yug_df.insert(
         len(yug_df.axes[1]),
         FREE_REST,
@@ -52,7 +56,7 @@ def main():
     yug_df.loc[yug_df[FREE_REST] < 0, FREE_REST] = 0
     yug_df.loc[yug_df[AVIALABLE_REST] < 0, AVIALABLE_REST] = 0
     yug_df = yug_df.merge(
-        filter_df[['EAN', 'Наименование']].drop_duplicates(subset=['EAN']),
+        filter_df[[EAN, NAME]].drop_duplicates(subset=[EAN]),
         on='EAN',
         how='left',
     )
