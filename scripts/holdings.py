161177bdd9886084b2dfd9513b72fe689c7ae592
alexey_ananchenko <ananchenko_as@mail.ru>
2022-11-19 19:54:06 +0300

    выделена отдельная папка со скриптами

---
+++ scripts/holdings.py
@@ -0,0 +1,52 @@
+"""
+Скрипт создаёт реестр соответствий кодов точек доставки,
+плательщиков, холдингов к основному холдингу
+
+"""
+
+import pandas as pd
+
+
+POINT = 'Точка доставки'
+PAYMENT = 'Плательщик'
+HOLDING = 'Холдинг'
+M_HOLDING = 'Основной холдинг'
+NAME_M_HOLDING = 'Наименование основного холдинга'
+CODES = 'Код холдинга'
+
+
+def main():
+    excel = pd.ExcelFile('../Исходники/Холдинги-Резервы.xlsx')
+    df_full = excel.parse('Точка доставки-Холдинг')
+    df = pd.concat([
+        df_full[[POINT, M_HOLDING, NAME_M_HOLDING]].rename(
+            columns={POINT: CODES}),
+        df_full[[PAYMENT, M_HOLDING, NAME_M_HOLDING]].rename(
+            columns={PAYMENT: CODES}),
+        df_full[[HOLDING, M_HOLDING, NAME_M_HOLDING]].rename(
+            columns={HOLDING: CODES})],
+        ignore_index=True
+    ).drop_duplicates()
+    result_data = {}
+    for column in df:
+        cleared_values = []
+        ch1 = '('
+        ch2 = ')'
+        for values in df[column]:
+            if ch1 in str(values):
+                cleared_values.append(
+                    values[(values.find(ch1) + 1):values.find(ch2)]
+                )
+            else:
+                cleared_values.append('Удалить строку')
+        result_data[column] = cleared_values
+    result_data[NAME_M_HOLDING] = df[NAME_M_HOLDING].to_list()
+    df_result = pd.DataFrame(result_data)
+    df_result[df_result[CODES] != 'Удалить строку'].to_excel(
+        '../Результаты/Холдинги.xlsx',
+        index=False
+    )
+
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/remains.py
@@ -0,0 +1,67 @@
+"""
+Скрипт подготавливает файл с остатками в удобном формате
+
+"""
+
+import pandas as pd
+
+
+FULL_REST = 'Полное наличие  (уч.ЕИ) '
+AVIALABLE_REST = 'Доступно (уч.ЕИ) '
+RESERVE = 'Мягкие + жёсткие резервы (уч.ЕД)'
+QUOTA = 'Остаток невыбранного резерва (уч.ЕИ)'
+FREE_REST = 'Свободный остаток (уч.ЕИ)'
+WHS = 'Склад'
+EAN = 'EAN'
+NAME = 'Наименование'
+LINK = 'Сцепка'
+WAREHOUSE = {
+    '800WHDIS': 'Краснодар',
+    '803WHDIS': 'Пятигорск',
+    '815WHDIS': 'Волгоград',
+    '800WHELB': 'Краснодар-ELB',
+    '803WHELB': 'Пятигорск-ELB'
+}
+
+
+def get_filtered_df(excel, dict_warehouses, skiprows=0):
+    full_df = excel.parse(skiprows=skiprows)
+    filter_df = full_df[full_df[WHS].isin(list(dict_warehouses.keys()))]
+    return filter_df.replace({WHS: dict_warehouses})
+
+
+def main():
+    xl = pd.ExcelFile(
+        '../Исходники/1082 - Доступность товара по складам (PG).xlsx'
+    )
+    filter_df = get_filtered_df(xl, WAREHOUSE)
+    filter_df.insert(
+        len(filter_df.axes[1]),
+        RESERVE,
+        filter_df[FULL_REST] - filter_df[AVIALABLE_REST]
+    )
+
+    yug_df = filter_df.groupby([WHS, EAN]).agg({
+        FULL_REST: 'sum',
+        RESERVE: 'sum',
+        AVIALABLE_REST: 'sum',
+        QUOTA: 'max'
+    }).reset_index()
+    yug_df.insert(0, LINK, yug_df[WHS] + yug_df[EAN].map(str))
+    yug_df.insert(
+        len(yug_df.axes[1]),
+        FREE_REST,
+        yug_df[AVIALABLE_REST] - yug_df[QUOTA]
+    )
+    yug_df.loc[yug_df[FREE_REST] < 0, FREE_REST] = 0
+    yug_df.loc[yug_df[AVIALABLE_REST] < 0, AVIALABLE_REST] = 0
+    yug_df = yug_df.merge(
+        filter_df[[EAN, NAME]].drop_duplicates(subset=[EAN]),
+        on='EAN',
+        how='left',
+    )
+    yug_df.to_excel('../Результаты/Остатки.xlsx', index=False)
+
+
+if __name__ == "__main__":
+    main()
