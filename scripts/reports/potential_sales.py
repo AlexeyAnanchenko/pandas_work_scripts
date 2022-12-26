b6bd3ca7d5e54c1669fa8cb7b7ce8c4b73520776
alexey_ananchenko <ananchenko_as@mail.ru>
2022-12-27 00:00:49 +0300

    добавил синхронизацию по факторам со многими клиентами

--- scripts/reports/potential_sales.py
+++ scripts/reports/potential_sales.py
@@ -5,10 +5,10 @@
 import utils
 utils.path_append()
 
-from service import save_to_excel, get_data, print_complete
+from service import save_to_excel, get_data, print_complete, replace_symbol
 from settings import REPORT_POTENTIAL_SALES, REPORT_DIR, TABLE_FACTORS
 from settings import FACTOR_PERIOD, FACTOR_STATUS, TABLE_SALES_HOLDINGS
-from settings import LINK_HOLDING
+from settings import LINK_HOLDING, NAME_HOLDING, TABLE_RESERVE, WHS, EAN
 
 
 def get_factors():
@@ -20,6 +20,22 @@ def get_factors():
 
 def add_col(df):
     sales, col_sales = get_data(TABLE_SALES_HOLDINGS)
+    reserves = get_data(TABLE_RESERVE)
+
+    for table in [df, sales, reserves]:
+        replace_symbol(table, NAME_HOLDING, ', ИП', ' ИП')
+    clients = list(set(df[NAME_HOLDING].to_list()))
+    mult_clients = [i for i in clients if ', ' in str(i)]
+    replace_holdings = {}
+    for mult_client in mult_clients:
+        for client in mult_client.split(', '):
+            replace_holdings[client] = mult_client
+    for table in [sales, reserves]:
+        table = table.replace({NAME_HOLDING: replace_holdings})
+        table[LINK_HOLDING] = (table[WHS]
+                               + table[NAME_HOLDING]
+                               + table[EAN].map(str))
+    df[LINK_HOLDING] = df[WHS] + df[NAME_HOLDING] + table[EAN].map(str)
     df = df.merge(
         sales[[LINK_HOLDING, col_sales['last_sale']]],
         on=LINK_HOLDING,
