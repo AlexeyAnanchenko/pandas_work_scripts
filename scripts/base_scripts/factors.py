05b7f5907c1cb7d59e1d1aeaf82518502fe7dae3
Alexey <ananchenko_as@mail.ru>
2023-01-08 22:12:35 +0300

    в факторах добавил изменение данных в случае всех холдингов

--- scripts/base_scripts/factors.py
+++ scripts/base_scripts/factors.py
@@ -17,10 +17,11 @@ from settings import FACTOR_STATUS, DATE_CREATION, DATE_START, NAME_HOLDING
 from settings import EAN, PRODUCT, LEVEL_3, DESCRIPTION, USER, PLAN_NFE
 from settings import FACT_NFE, ADJUSTMENT_PBI, SALES_PBI, RESERVES_PBI
 from settings import CUTS_PBI, LINK, LINK_HOLDING, PURPOSE_PROMO
-from settings import ALL_CLIENTS, TOTAL_RSV
-from settings import TABLE_SALES_HOLDINGS, TABLE_RESERVE
-# from update_data import update_factors_nfe, update_factors_pbi
-# from update_data import update_factors_nfe_promo
+from settings import ALL_CLIENTS, TOTAL_RSV, SALES_FACTOR_PERIOD, NAME_TRAD
+from settings import AVG_FACTOR_PERIOD, RSV_FACTOR_PERIOD
+from settings import TABLE_SALES_HOLDINGS, TABLE_RESERVE, TABLE_SALES
+from update_data import update_factors_nfe, update_factors_pbi
+from update_data import update_factors_nfe_promo
 
 
 SOURCE_FILE = 'NovoForecastServer_РезультатыПоиска.xlsx'
@@ -51,6 +52,14 @@ DATE_EXPIRATION_LOC = 'Дата окончания действия фактор
 DESCRIPTION_LOC = 'Описание'
 NUMBER_PROMO = 'Номер'
 PURPOSE = 'Цель'
+PAST = 'Прошедший'
+CURRENT = 'Текущий'
+FUTURE = 'Будущий'
+TRAD_HOLDINGS = [
+    'Васильева Татьяна Викторовна ИП (8108456)',
+    'Воронина Елена Сергеевна ИП (8179862)',
+    'Петровская Татьяна Анатольевна ИП (8143465)'
+]
 
 
 def filtered_factors(file=SOURCE_FILE, column=FACTOR, skip=0):
@@ -116,15 +125,16 @@ def split_by_month(df):
     current_month_dt = dt.datetime.combine(current_month, dt.time(0, 0))
     next_month = today + relativedelta.relativedelta(months=1, day=1)
     next_month_dt = dt.datetime.combine(next_month, dt.time(0, 0))
-    df[FACTOR_PERIOD] = 'Будущий'
+    df[FACTOR_PERIOD] = FUTURE
     idx = df[df[DATE_EXPIRATION_LOC] < next_month_dt].index
-    df.loc[idx, FACTOR_PERIOD] = 'Текущий'
+    df.loc[idx, FACTOR_PERIOD] = CURRENT
     idx = df[df[DATE_EXPIRATION_LOC] < current_month_dt].index
-    df.loc[idx, FACTOR_PERIOD] = 'Прошедший'
+    df.loc[idx, FACTOR_PERIOD] = PAST
     return df
 
 
 def reindex_rename(df):
+    """Упорядочивает и переименовывает заголовок таблицы"""
     result_df = df[[
         LINK, LINK_HOLDING, FACTOR_LOC, REF_FACTOR_LOC, FACTOR_NUM,
         FACTOR_STATUS_LOC, PURPOSE, FACTOR_PERIOD, DATE_CREATION_LOC,
@@ -144,7 +154,35 @@ def reindex_rename(df):
     return result_df
 
 
+def group_mult_clients(df, mult_clients, static_col, numeric_col):
+    """
+    Функция обновляет холдинги в dataframe по заданным мульти-холдингам
+    и возвращает его сгруппированным
+    """
+    replace_holdings = {}
+    for mult_client in mult_clients:
+        mult_client_list = mult_client.split('), ')
+
+        for client in mult_client_list:
+            if client != mult_client_list[len(mult_client_list) - 1]:
+                replace_holdings[client + ')'] = mult_client
+            else:
+                replace_holdings[client] = mult_client
+
+    df = df.replace({NAME_HOLDING: replace_holdings})
+    df = df.drop(columns=[LINK_HOLDING], axis=1)
+    df.insert(0, LINK_HOLDING, df[WHS] + df[NAME_HOLDING] + df[EAN].map(str))
+    numeric_col_dict = {}
+
+    for col in numeric_col:
+        numeric_col_dict[col] = 'sum'
+
+    df = df.groupby(static_col).agg(numeric_col_dict).reset_index()
+    return df
+
+
 def add_sales_and_rsv(df):
+    """Подтягивает продажи и резервы в рамках определённого периода фактора"""
     sales, col_sales = get_data(TABLE_SALES_HOLDINGS)
     rsv = get_data(TABLE_RESERVE)
     static_col = [LINK, LINK_HOLDING, WHS, EAN, NAME_HOLDING]
@@ -157,30 +195,75 @@ def add_sales_and_rsv(df):
     mult_clients = [i for i in clients if '), ' in str(i)]
 
     if mult_clients:
-        sales = utils.group_mult_clients(
+        sales = group_mult_clients(
             sales, mult_clients, static_col, num_col_sales)
-        rsv = utils.group_mult_clients(
+        rsv = group_mult_clients(
             rsv, mult_clients, static_col, num_col_rsv)
 
+    df = df.merge(sales[static_col + num_col_sales], on=static_col, how='left')
+    df = df.merge(rsv[static_col + num_col_rsv], on=static_col, how='left')
+    idx = df[df[FACTOR_PERIOD] == CURRENT].index
+    df.loc[idx, SALES_FACTOR_PERIOD] = df.loc[idx, col_sales['last_sale']]
+    idx = df[df[FACTOR_PERIOD] == PAST].index
+    df.loc[idx, SALES_FACTOR_PERIOD] = df.loc[idx, col_sales['pntm_sale']]
+    idx = df[df[FACTOR_PERIOD] == FUTURE].index
+    df.loc[idx, SALES_FACTOR_PERIOD] = 0
+    df[AVG_FACTOR_PERIOD] = df[col_sales['avg_cut_sale']]
+    df[RSV_FACTOR_PERIOD] = df[TOTAL_RSV]
+    df.drop(col_sales['last_sale'], axis=1, inplace=True)
+    df.drop(col_sales['pntm_sale'], axis=1, inplace=True)
+    df.drop(col_sales['avg_cut_sale'], axis=1, inplace=True)
+    df.drop(TOTAL_RSV, axis=1, inplace=True)
+    return df
+
+
+def replace_general_sales(df):
+    """
+    Заменяет данные полученные в 'add_sales_and_rsv', где холдинги не
+    указаны, либо где холдинги традиционной торговли
+    """
+    sales, col_sales = get_data(TABLE_SALES)
+    TRAD_HOLDINGS.append(ALL_CLIENTS)
     df = df.merge(
-        sales[static_col + num_col_sales],
-        on=static_col,
+        sales[[
+            LINK, col_sales['pntm_sale'],
+            col_sales['last_sale'], col_sales['avg_cut_sale']
+        ]],
+        on=LINK,
         how='left'
     )
-    df = df.merge(
-        rsv[static_col + num_col_rsv],
-        on=static_col,
-        how='left'
-    )
+    idx = df[
+        (df[FACTOR_PERIOD] == CURRENT) & (df[NAME_HOLDING].isin(TRAD_HOLDINGS))
+    ].index
+    df.loc[idx, SALES_FACTOR_PERIOD] = df.loc[idx, col_sales['last_sale']]
+    idx = df[
+        (df[FACTOR_PERIOD] == PAST) & (df[NAME_HOLDING].isin(TRAD_HOLDINGS))
+    ].index
+    df.loc[idx, SALES_FACTOR_PERIOD] = df.loc[idx, col_sales['pntm_sale']]
+    idx = df[
+        (df[FACTOR_PERIOD] == FUTURE) & (df[NAME_HOLDING].isin(TRAD_HOLDINGS))
+    ].index
+    df.loc[idx, SALES_FACTOR_PERIOD] = 0
+
+    idx = df[df[NAME_HOLDING].isin(TRAD_HOLDINGS)].index
+    df.loc[idx, AVG_FACTOR_PERIOD] = df.loc[idx, col_sales['avg_cut_sale']]
+    df.drop(col_sales['last_sale'], axis=1, inplace=True)
+    df.drop(col_sales['pntm_sale'], axis=1, inplace=True)
+    df.drop(col_sales['avg_cut_sale'], axis=1, inplace=True)
+
+    TRAD_HOLDINGS.remove(ALL_CLIENTS)
+    idx = df[df[NAME_HOLDING].isin(TRAD_HOLDINGS)].index
+    df.loc[idx, NAME_HOLDING] = NAME_TRAD
     return df
 
 
 def main():
-    # update_factors_nfe(SOURCE_FILE)
-    # update_factors_nfe_promo(SOURCE_FILE_PROMO)
-    # update_factors_pbi(SOURCE_FILE_PB)
+    update_factors_nfe(SOURCE_FILE)
+    update_factors_nfe_promo(SOURCE_FILE_PROMO)
+    update_factors_pbi(SOURCE_FILE_PB)
     factors = add_pbi_and_purpose(add_num_factors(filtered_factors()))
     factors = add_sales_and_rsv(reindex_rename(split_by_month(factors)))
+    factors = replace_general_sales(factors)
     save_to_excel(RESULT_DIR + TABLE_FACTORS, factors)
     print_complete(__file__)
 
--- scripts/base_scripts/utils.py
+++ scripts/base_scripts/utils.py
@@ -4,14 +4,10 @@
 
 from sys import path
 from os.path import dirname
-path.append(dirname(dirname(__file__)))
-
-from settings import NAME_HOLDING, LINK_HOLDING, WHS, EAN
 
 
 def path_append():
     """Функция добавляет папку с настройками и сервисом в область видимости"""
-
     path.append(dirname(dirname(__file__)))
 
 
@@ -19,31 +15,4 @@ def void_to(df, col, value):
     """Заменяет пустые значения в столбце на указанное значение"""
     idx = df[df[col].isnull()].index
     df.loc[idx, col] = value
-    return df
-
-
-def group_mult_clients(df, mult_clients, static_col, numeric_col):
-    """
-    Функция обновляет холдинги в dataframe по заданным мульти-холдингам
-    и возвращает его сгруппированным
-    """
-    replace_holdings = {}
-    for mult_client in mult_clients:
-        mult_client_list = mult_client.split('), ')
-
-        for client in mult_client_list:
-            if client != mult_client_list[len(mult_client_list) - 1]:
-                replace_holdings[client + ')'] = mult_client
-            else:
-                replace_holdings[client] = mult_client
-
-    df = df.replace({NAME_HOLDING: replace_holdings})
-    df = df.drop(columns=[LINK_HOLDING], axis=1)
-    df.insert(0, LINK_HOLDING, df[WHS] + df[NAME_HOLDING] + df[EAN].map(str))
-    numeric_col_dict = {}
-
-    for col in numeric_col:
-        numeric_col_dict[col] = 'sum'
-
-    df = df.groupby(static_col).agg(numeric_col_dict).reset_index()
     return df
