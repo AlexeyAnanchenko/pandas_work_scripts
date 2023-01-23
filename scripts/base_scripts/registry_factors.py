eeb56568ba1fd298c09d3b0041775028f6fd2b48
Alexey <ananchenko_as@mail.ru>
2023-01-23 15:37:12 +0300

    реализовал удаление дубликатов в течение дня

--- scripts/base_scripts/registry_factors.py
+++ scripts/base_scripts/registry_factors.py
@@ -17,6 +17,12 @@ from service import get_data, save_to_excel, print_complete
 LINK_FACTOR_NUM = 'Сцепка Номер фактора-Склад-ШК'
 PLAN_IN_NFE = 'План в NFE, шт'
 VARIANCE = 'Различие, шт'
+LINK_DATE = 'Сцепка с датой регистриации'
+COL_REPORT = [
+    LINK_FACTOR_NUM, LINK, FACTOR_PERIOD, FACTOR_NUM, DATE_CREATION,
+    DATE_START, DATE_EXPIRATION, WHS, NAME_HOLDING, EAN, PRODUCT,
+    PLAN_IN_NFE, DATE_REGISTRY, QUANT_REGISTRY
+]
 
 
 def check_present_registry():
@@ -27,11 +33,7 @@ def check_present_registry():
 
 
 def create_registry():
-    df = pd.DataFrame(columns=[
-        LINK_FACTOR_NUM, LINK, FACTOR_PERIOD, FACTOR_NUM, DATE_CREATION,
-        DATE_START, DATE_EXPIRATION, WHS, NAME_HOLDING, EAN, PRODUCT,
-        PLAN_IN_NFE, DATE_REGISTRY, QUANT_REGISTRY
-    ])
+    df = pd.DataFrame(columns=COL_REPORT)
     save_to_excel(RESULT_DIR + TABLE_REGISTRY_FACTORS, df)
     return
 
@@ -61,14 +63,14 @@ def fix_changes():
     df_fct.loc[df_fct[PLAN_IN_NFE].isnull(), PLAN_IN_NFE] = 0
     df_fct[VARIANCE] = df_fct[PLAN_NFE] - df_fct[PLAN_IN_NFE]
     df_var = df_fct[df_fct[VARIANCE] != 0].copy()
-    df_var[DATE_REGISTRY] = pd.Timestamp.today()
-    idx = df_var[df_var[PLAN_IN_NFE] != 0].index
-    df_var.loc[idx, DATE_CREATION] = df_var.loc[idx, DATE_REGISTRY]
+    df_var[DATE_REGISTRY] = pd.Timestamp(pd.Timestamp.today().date())
     df_var[QUANT_REGISTRY] = df_var[VARIANCE]
     df_var = df_var.drop(labels=[PLAN_NFE, VARIANCE], axis=1)
     df_rgs = pd.concat([df_rgs, df_var], ignore_index=True)
 
-    change_col = [FACTOR_PERIOD, DATE_START, DATE_EXPIRATION, PLAN_IN_NFE]
+    change_col = [
+        FACTOR_PERIOD, DATE_CREATION, DATE_START, DATE_EXPIRATION, PLAN_IN_NFE
+    ]
     df_rgs = df_rgs.drop(labels=change_col, axis=1)
     df_fct = df_fct.drop(labels=[PLAN_IN_NFE], axis=1)
     df_fct = df_fct.rename(columns={PLAN_NFE: PLAN_IN_NFE})
@@ -78,10 +80,22 @@ def fix_changes():
     return df_rgs
 
 
+def group_duplicates(df):
+    df[LINK_DATE] = df[LINK_FACTOR_NUM] + df[DATE_REGISTRY].map(str)
+    df_group = df[[LINK_DATE, QUANT_REGISTRY]].groupby([LINK_DATE]).agg(
+        {QUANT_REGISTRY: 'sum'}
+    ).reset_index()
+    df = df.drop(labels=[QUANT_REGISTRY], axis=1)
+    df = df.merge(df_group, on=LINK_DATE, how='left')
+    df = df.drop(labels=[LINK_DATE], axis=1)
+    df = df.reindex(columns=COL_REPORT).drop_duplicates()
+    return df
+
+
 def main():
     if check_present_registry():
         create_registry()
-    df = fix_changes()
+    df = group_duplicates(fix_changes())
     save_to_excel(RESULT_DIR + TABLE_REGISTRY_FACTORS, df)
     print_complete(__file__)
 
