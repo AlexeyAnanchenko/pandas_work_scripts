c25030b2ad6bef119b293e403350940830a7d344
Alexey <ananchenko_as@mail.ru>
2023-03-03 17:59:04 +0300

    добавлены расчётные столбцы НЕ ПРОДАНО

--- scripts/reports/tracking_not_sold.py
+++ scripts/reports/tracking_not_sold.py
@@ -13,7 +13,7 @@ from service import get_mult_clients_dict
 from settings import REPORT_NOT_SOLD_PAST, REPORT_DIR, TABLE_FACTORS, PAST
 from settings import CURRENT, FACTOR_PERIOD, PURPOSE_PROMO, INACTIVE_PURPOSE
 from settings import SALES_FACTOR_PERIOD, LINK, LINK_HOLDING, WHS, NAME_HOLDING
-from settings import EAN, PRODUCT, LEVEL_3, PLAN_NFE, ADJUSTMENT_PBI, FACT_NFE
+from settings import EAN, PRODUCT, LEVEL_3
 from settings import DESCRIPTION, USER, SALES_CURRENT_FOR_PAST, TABLE_REMAINS
 from settings import RSV_FACTOR_PERIOD_TOTAL, TABLE_PURCHASES, FREE_REST
 from settings import ARCHIVE_DIR, FULL_REST, SOFT_HARD_RSV, QUOTA, OVERSTOCK
@@ -33,20 +33,23 @@ SALES_START_FROM_MONTH = ('Продажи клиента (-ов) начиная 
                           ' месяца подачи фактора, шт')
 SALES_WHS_START_FROM_MONTH = ('Продажи по складу после окончания'
                               ' действия фактора, шт')
-
-LINK_HOLDING_PERIOD = 'Сцепка Склад-Холдинг-Штрихкод-Период'
-LINK_PERIOD = 'Сцепка Склад-Штрихкод-Период'
-PLAN = 'План, шт'
-SALES_TOTAL = 'Продажи Тотал, шт'
-SALES = 'Продажи в пределах фактора, шт'
+PLAN_NFE = 'План в NFE, шт'
+ADJUSTMENT_NFE = 'Корректировка в NFE, шт'
+PURCHASES = 'Закупки в течение месяца, шт'
+FULL_REST_MONTH = 'Сток на 1 число следующего месяца, шт'
+MONTH = 'Месяц'
+PLAN = 'Максимальный План, шт'
+SALES = 'Продажи в пределах объёма фактора, шт'
 RSV = 'Резервы (с квотой), шт'
-FREE_REST_CURRENT = 'Свободные остатки текущего месяца, шт'
-PURCH_PAST = 'Закупки прошлого месяца, шт'
-PURCH_CURRENT = 'Закупки текущего месяца, шт'
-FREE_REST_PAST = 'Свободные остатки для факторов прошедшего периода, шт'
+FREE_REST_CALC = 'Свободный остаток для расчёта, шт'
+OVERSTOCK_CALC = OVERSTOCK + '(Для расчёта)'
+FULL_REST_CALC = 'Полный остаток для расчёта, шт'
 PLAN_MINUS_SALES = 'План - Продажи, шт'
 PLAN_MINUS_SALES_RSV = 'План - Продажи - Резервы, шт'
+LINK_PERIOD = 'Сцепка Месяц фактора-Склад-ШК'
+
 RSV_BY_PLAN = 'Резерв по Плану, шт'
+RSV_BY_PLAN_DUBLE = 'Резерв по Плану, шт (копия)'
 RSV_BY_PLAN_TOTAL = 'В резерве по плану всего, шт'
 RSV_BY_PLAN_PURCH = 'Резерв по Плану (с учётом закупа), шт'
 PLAN_MINUS_SALES_RSV_TOTAL = 'План - Продажи - Резервы по сцепке, шт'
@@ -193,8 +196,137 @@ def merge_reserve(df):
     return df
 
 
+def gen_plan_sales_rsv(df):
+    """Формируем итоговый столбцы плана и продаж"""
+    df[PLAN] = np.maximum(df[PLAN_NFE], df[ADJUSTMENT_NFE]).round(0)
+    df[SALES] = df[[PLAN, SALES_START_FROM_MONTH]].min(axis=1)
+    df = df.rename(columns={TOTAL_RSV: RSV})
+    return df
+
+
+def merge_remains(df):
+    """Подтягивает остатки и закупки для расчёта"""
+    df_rem = get_data(TABLE_REMAINS)[[
+        LINK, FREE_REST, SOFT_HARD_RSV, QUOTA, OVERSTOCK, FULL_REST
+    ]]
+    df = df.merge(df_rem, on=LINK, how='left')
+    for col in [FREE_REST, SOFT_HARD_RSV, QUOTA, OVERSTOCK, FULL_REST]:
+        df = utils.void_to(df, col, 0)
+
+    df[FREE_REST_CALC] = np.minimum(
+        np.maximum(
+            (df[FULL_REST_MONTH] - df[SALES_WHS_START_FROM_MONTH]
+             - df[SOFT_HARD_RSV] - df[QUOTA]),
+            0
+        ),
+        df[FREE_REST]
+    )
+    df[FULL_REST_CALC] = np.minimum(
+        np.maximum(df[FULL_REST_MONTH] - df[SALES_WHS_START_FROM_MONTH], 0),
+        df[FULL_REST]
+    )
+    df[OVERSTOCK_CALC] = df[[OVERSTOCK, FULL_REST_CALC]].min(axis=1)
+    return df
+
+
+def merge_group_col(df, group_col, new_name_col):
+    """Подтягивает сгруппированный и переименованный столбец """
+    group_df = df.merge(
+        df[[LINK_PERIOD, group_col]].groupby([LINK_PERIOD]).agg({
+            group_col: 'sum'
+        }).reset_index().rename(columns={group_col: new_name_col}),
+        on=LINK_PERIOD, how='left'
+    )
+    return group_df
+
+
+def merge_group_rsv(df):
+    """Подтягивает сгруппированный и переименованный столбец по резервам"""
+    df_copy = df.copy()
+    idx = df_copy[df_copy[NAME_HOLDING].isin([ALL_CLIENTS, NAME_TRAD])].index
+    df_copy.loc[idx, RSV_BY_PLAN] = 0
+    group_df = df.merge(
+        df_copy[[LINK_PERIOD, RSV_BY_PLAN]].groupby([LINK_PERIOD]).agg({
+            RSV_BY_PLAN: 'sum'
+        }).reset_index().rename(columns={RSV_BY_PLAN: RSV_BY_PLAN_TOTAL}),
+        on=LINK_PERIOD, how='left'
+    )
+    df_copy = df[df[NAME_HOLDING].isin([ALL_CLIENTS, NAME_TRAD])].copy()
+    temp_col = RSV_BY_PLAN + '2'
+    df_copy = df_copy.groupby([LINK_PERIOD]).agg({
+        RSV_BY_PLAN: 'max'
+    }).reset_index().rename(columns={RSV_BY_PLAN: temp_col})
+    group_df = group_df.merge(
+        df_copy[[LINK_PERIOD, temp_col]], on=LINK_PERIOD, how='left'
+    )
+    idx = group_df[~group_df[temp_col].isnull()].index
+    group_df.loc[idx, RSV_BY_PLAN_TOTAL] = np.maximum(
+        group_df.loc[idx, RSV_BY_PLAN_TOTAL], group_df.loc[idx, temp_col]
+    )
+    group_df = group_df.drop(columns=[temp_col], axis=1)
+    return group_df
+
+
+def not_sold(df):
+    """Расчёт показателей недогруза"""
+    df[LINK_PERIOD] = df[LINK] + df[MONTH]
+    df[PLAN_MINUS_SALES] = np.maximum(df[PLAN] - df[SALES], 0)
+    df[RSV_BY_PLAN] = df[[RSV, PLAN_MINUS_SALES]].min(axis=1)
+    df = merge_group_rsv(df)
+    df[RSV_BY_PLAN_PURCH] = (
+        df[RSV_BY_PLAN] / df[RSV_BY_PLAN_TOTAL]
+        * df[[PURCHASES, RSV_BY_PLAN_TOTAL]].min(axis=1)
+    ).round(0)
+    df = utils.void_to(df, RSV_BY_PLAN_PURCH, 0)
+
+    df[PLAN_MINUS_SALES_RSV] = np.maximum(df[PLAN] - df[SALES] - df[RSV], 0)
+    df = merge_group_col(df, PLAN_MINUS_SALES_RSV, PLAN_MINUS_SALES_RSV_TOTAL)
+    df[FREE_REST_BY_PLAN] = (
+        df[PLAN_MINUS_SALES_RSV] / df[PLAN_MINUS_SALES_RSV_TOTAL]
+        * df[[PLAN_MINUS_SALES_RSV_TOTAL, FREE_REST_CALC]].min(axis=1)
+    ).round(0)
+    df = utils.void_to(df, FREE_REST_BY_PLAN, 0)
+    df = merge_group_col(df, FREE_REST_BY_PLAN, FREE_REST_BY_PLAN_TOTAL)
+    df[FREE_REST_BY_PLAN_PURCH] = (
+        df[FREE_REST_BY_PLAN] / df[FREE_REST_BY_PLAN_TOTAL]
+        * df[[FREE_REST_BY_PLAN_TOTAL, PURCHASES]].min(axis=1)
+    ).round(0)
+    df = utils.void_to(df, FREE_REST_BY_PLAN_PURCH, 0)
+
+    df[NOT_SOLD] = df[RSV_BY_PLAN] + df[FREE_REST_BY_PLAN]
+    df[NOT_SOLD_PURCH] = df[RSV_BY_PLAN_PURCH] + df[FREE_REST_BY_PLAN_PURCH]
+    return df
+
+
+# def responsibility_for_overstock(df):
+#     """Расчёт вклад в пересток по заказчикам"""
+#     df = merge_group_col(df, NOT_SOLD, NOT_SOLD_TOTAL)
+#     df = merge_group_col(df, NOT_SOLD_PURCH, NOT_SOLD_PURCH_TOTAL)
+#     df[RES_FOR_STOCK] = (
+#         df[NOT_SOLD] / df[NOT_SOLD_TOTAL]
+#         * df[[NOT_SOLD_TOTAL, OVERSTOCK]].min(axis=1)
+#     ).round(0)
+#     df = utils.void_to(df, RES_FOR_STOCK, 0)
+#     df[RES_FOR_STOCK_PURCH] = (
+#         df[NOT_SOLD_PURCH] / df[NOT_SOLD_PURCH_TOTAL]
+#         * df[[NOT_SOLD_PURCH_TOTAL, OVERSTOCK]].min(axis=1)
+#     ).round(0)
+#     df = utils.void_to(df, RES_FOR_STOCK_PURCH, 0)
+#     return df
+
+
+# def merge_directory(df):
+#     """Добавляем данные из справочника"""
+#     df_dir = get_data(TABLE_DIRECTORY)[[EAN, MSU, BASE_PRICE]]
+#     df = df.merge(df_dir, on=EAN, how='left')
+#     df = utils.void_to(df, MSU, 0)
+#     df = utils.void_to(df, BASE_PRICE, 0)
+#     return df
+
+
 def main():
-    df = merge_reserve(merge_sales(get_factors()))
+    df = gen_plan_sales_rsv(merge_reserve(merge_sales(get_factors())))
+    df = not_sold(merge_remains(df))
     save_to_excel(REPORT_DIR + REPORT_TRACKING_NOT_SOLD, df)
     print_complete(__file__)
 
